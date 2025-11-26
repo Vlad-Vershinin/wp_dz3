33ec25c44f482f0b71e3f3934390871272905630
Vlad-Vershinin <vladversinin676@gmail.com>
2025-11-26 20:11:46 +0500

    inital commit

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77c8cd98823a3a173948ebceeff68b7fcb48d86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02361fc3641bca057764ec4491fcfdf7c79aa00f")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
